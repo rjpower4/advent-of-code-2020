3ab6b872f7ba31cb65b43fe50d8681579f50cccf
Rolfe Power <rpower@purdue.edu>
2020-12-07 01:45:52 -0500

    Remove unnecessary arrays

--- day-06/day-06.jl
+++ day-06/day-06.jl
@@ -4,14 +4,14 @@ const INPUT_FILE="input.txt"
 
 function solve(ifile=INPUT_FILE)
     families = read(ifile, String) |> x-> split(x, "\n\n")
-    part_1 = zeros(Int, length(families))
-    part_2 = zeros(Int, length(families))
+    part_1 = 0
+    part_2 = 0
     for (i, f) in enumerate(families)
-        k = split(f, '\n')
-        part_1[i] = length(mapreduce(Set, union, k))
-        part_2[i] = length(mapreduce(Set, intersect, k))
+        sets = split(f, '\n') |> x -> map(Set, x)
+        part_1 += length(reduce(union, sets))
+        part_2 += length(reduce(intersect, sets))
     end
-    (p1=sum(part_1), p2=sum(part_2))
+    (p1=part_1, p2=part_2)
 end
 
 end
